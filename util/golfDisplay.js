b35e83ae168e9cc2f311113de1026de4baf45286
Auddity <audsecondarylap@outlook.com>
2022-03-12 11:07:24 -0500

    input columns

--- util/golfDisplay.js
+++ util/golfDisplay.js
@@ -1,37 +1,58 @@
 import get from '../util/getElement.js';
 
+const gridDisplay = get('grid-display');
+
+const createInputCell = () => {
+  const scoreInputEl = document.createElement('input');
+  scoreInputEl.className = 'cell scoreInput';
+  scoreInputEl.setAttribute('type', 'text');
+  gridDisplay.appendChild(scoreInputEl);
+}
+
+const createDisplayCell = (y) => {
+  const cellEl = document.createElement('div');
+  cellEl.className = 'cell';
+
+  cellEl.textContent = `${y}`;
+
+  gridDisplay.appendChild(cellEl);
+}
+
+const createLabelRow  = x => {
+  const labelEl = document.createElement('div');
+  labelEl.className = 'cell label';
+  labelEl.style.background = 'green';
+  if(x === 0) labelEl.textContent = 'Yardage';
+  if(x === 1) labelEl.textContent = 'Handicap';
+  if(x === 4) labelEl.textContent = 'Hole';
+  if(x === 7) labelEl.textContent = 'Par';
+  if(x === 0 || x === 1 || x === 4 || x === 7) labelEl.classList.add('label');
+  if(x === 2) labelEl.setAttribute('id', 'player1');
+  if(x === 3) labelEl.setAttribute('id', 'player2');
+  if(x === 4) labelEl.setAttribute('id', 'player3');
+  if(x === 5) labelEl.setAttribute('id', 'player4');
+  gridDisplay.appendChild(labelEl);
+}
+
+
 const createGrid = () => {
-  let cells = 168;
-  let base = 10;
-  let increment = 8;
-  const gridDisplay = get('grid-display');
+  let height = 20;
+  let width = 8;
+
+  for(let i = 0; i < width; i++) {
+    createLabelRow(i);
+  }
   
-    // const scoreInputEl = document.createElement('input');
-  //   scoreInputEl.className = 'cell scoreInput';
-  //   scoreInputEl.setAttribute('type', 'text');
-  //   scoreInputEl.style.background = 'red';
-  //   gridDisplay.appendChild(scoreInputEl);
-  
-  for(let i = 0; i < cells; i++) {
-    const cellEl = document.createElement('div');
-    cellEl.className = 'cell';
-    if(i === 0) cellEl.textContent = 'Yardage';
-    if(i === 1) cellEl.textContent = 'Handicap';
-    if(i === 4) cellEl.textContent = 'Hole';
-    if(i === 7) cellEl.textContent = 'Par';
-    if(i === 0 || i === 1 || i === 4 || i === 7) cellEl.classList.add('label');
-    if(i === 2) cellEl.setAttribute('id', 'player1');
-    if(i === 3) cellEl.setAttribute('id', 'player2');
-    if(i === 4) cellEl.setAttribute('id', 'player3');
-    if(i === 5) cellEl.setAttribute('id', 'player4');
-
-    if(i >= 8) cellEl.textContent = `${i}`;
-
-
-
-    gridDisplay.appendChild(cellEl);
-    
-    
+  for(let y = 0; y < height; y++) {
+    for(let x = 0; x < width; x++) {
+      let inputColumn = x + 1;
+      
+      if(inputColumn === 3 || inputColumn === 4 || inputColumn === 6 || inputColumn === 7) {
+        createInputCell();
+      } else {
+        createDisplayCell(inputColumn);
+      }
+    }
   }
 
 
