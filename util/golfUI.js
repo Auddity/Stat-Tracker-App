d57ab63863b7cd364423a1c1ccfc21f73475a048
Auddity <audsecondarylap@outlook.com>
2022-03-20 14:54:28 -0400

    display inputed names

--- util/golfUI.js
+++ util/golfUI.js
@@ -5,21 +5,20 @@ const input = get('golfer-name');
 // UI (display)
 export default class UI {
   updateDom() {
-    
     const golferOne = get('golfer1');
     const golferTwo = get('golfer2');
     const golferThree = get('golfer3');
     const golferFour = get('golfer4');
-
     const golfers = [golferOne, golferTwo, golferThree, golferFour];
     const name = input.value;
-
-
-    for(let el of golfers) {
-      if(el.textContent === '') golfers.textContent = `${name}`;
+    for (let el of golfers) {
+      if(el.textContent === '') {
+        el.textContent = `${name}`;
+        break;
+      } else {
+        console.log('foursome already full');
+      }
     }
-  
-
     input.value = '';
   }
 }
